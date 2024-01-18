94ecf6cb3c962cb35d6ac97231790c068783b958
M-DEV77 <marcceloolimpiofbc@gmail.com>
2024-01-18 16:53:21 -0300

    criando modulo user part1

---
+++ backEnd/db/conn.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+async function main(){
+    await mongoose.connect( 'mongodb://localhost:27017/engapp',()=>{
+        console.log('conectou ao banco de dados!!')
+    });
+}
+main().catch((err)=>console.log(err));
+
+module.exports = mongoose;
---
+++ backEnd/models/userModel.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+const User = mongoose.model();
+module.exports = User;
